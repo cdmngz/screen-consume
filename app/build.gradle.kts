f653222a70dffc7974d298e73ff70ca6e7f0dd07
cdmngz <5540259+cdmngz@users.noreply.github.com>
2026-08-18 17:46:04 +0200

    Prepare v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.screenconsume.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 1
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
